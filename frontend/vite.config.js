b95657d796991af6bc156f0802ce12fc4817398a
Josh <joshokeeffecollege@gmail.com>
2025-12-03 13:48:48 +0000

    CSP Headers

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,21 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     server: {
+        headers: {
+            "Content-Security-Policy":
+                [
+                    "default-src 'self';",
+                    "script-src 'self';",
+                    "style-src 'self';",
+                    "img-src 'self' data:;",
+                    "object-src 'none';",
+                    "base-uri 'self';",
+                    "form-action 'self';",
+                    "frame-ancestors 'none';"
+                ].join(" "),
+            "X-Frame-Options": "DENY",
+            "X-Content-Type-Options": "nosniff"
+        },
         proxy: {
             '/api': 'http://localhost:5000',
         },
