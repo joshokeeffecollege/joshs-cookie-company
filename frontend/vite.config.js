b619ad0eccbdc9fa279b13d1f6bd268cf4ff7bea
Josh <joshokeeffecollege@gmail.com>
2025-12-05 16:07:19 +0000

    CSP Error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,23 +4,24 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     server: {
-        headers: {
-            "Content-Security-Policy":
-                [
-                    "default-src 'self';",
-                    "script-src 'self';",
-                    "style-src 'self';",
-                    "img-src 'self' data:;",
-                    "object-src 'none';",
-                    "base-uri 'self';",
-                    "form-action 'self';",
-                    "frame-ancestors 'none';"
-                ].join(" "),
-            "X-Frame-Options": "DENY",
-            "X-Content-Type-Options": "nosniff"
-        },
         proxy: {
             '/api': 'http://localhost:5000',
         },
     },
 });
+
+// headers: {
+//     "Content-Security-Policy":
+//     [
+//         "default-src 'self';",
+//         "script-src 'self';",
+//         "style-src 'self';",
+//         "img-src 'self' data:;",
+//         "object-src 'none';",
+//         "base-uri 'self';",
+//         "form-action 'self';",
+//         "frame-ancestors 'none';"
+//     ].join(" "),
+//         "X-Frame-Options": "DENY",
+//         "X-Content-Type-Options": "nosniff"
+// },
